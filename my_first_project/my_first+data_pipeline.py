4734538704b390e48c0f5c1f24b2b8ae3cd9dc5f
DollarWittawat <112494241+DollarWittawat@users.noreply.github.com>
2024-01-27 02:22:48 +0000

    Create Dog Api Pipeline using Airflow

---
+++ my_first_project/my_first+data_pipeline.py
@@ -0,0 +1,33 @@
+import json
+
+import requests
+
+
+if __name__ == "__main__":
+    # Get data from Dog API
+    # โค้ดส่วนนี้จะเป็นการเชื่อมต่อกับ Dog API โดยเราจะใส่ URL Endpoint
+    url = "https://dog.ceo/api/breeds/image/random"
+    # เสร็จแล้วจะยิง Request ไปดึงข้อมูลมาเก็บใส่ Response
+    response = requests.get(url)
+    data = response.json()
+    print(data)
+
+    # Write data to file
+    with open("dogs.json", "w") as f:
+        json.dump(data, f)
+
+    # โค้ดส่วนนี้จะเป็นการเตรียมเรื่อง Credentials และ Configuration ต่าง ๆ เพื่อใช้ในการเชื่อมต่อกับ Dog API
+    api_url = "https://api.jsonbin.io/v3/b"
+    headers = {
+        "Content-Type": "application/json",
+        "X-Master-Key": "$2a$10$DhjAfQd7WAeg/fWuSs5yDOW/qvYxY/pG1yVeDawI2pxrqjt92uAke",
+        "X-Collection-Id": "65ab445a266cfc3fde7c6f2a",
+    }
+
+    # Read data from file
+    with open("dogs.json", "r") as f:
+        data = json.load(f)
+
+    # โค้ดส่วนนี้จะเป็นการยิง Request ไปเพื่ออัพโหลดข้อมูลไปยังเซิฟเวอร์ของ JSONBin
+    response = requests.post(api_url, json=data, headers=headers)
+    print(response.json())
